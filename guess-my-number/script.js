1c96d2b0de741f2964c07e63b0fe792580b4cb6f
Trần Quốc Tài - CMC Global DU3 <tqtai1@ldap.cmcglobal.com.vn>
2021-03-12 11:22:05 +0700

    12-3-2021

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -11,17 +11,85 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 23;
 */
 
-const secretNumber = Math.trunc(Math.random()*20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+var highscore = Number(document.querySelector('.highscore').textContent);
+var secretNumber = Math.trunc(Math.random()*20) + 1;
+var score = Number(document.querySelector('.score').textContent);
+
+console.log(score);
 
 document.querySelector('.check').addEventListener
 ('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
     if(!guess) {
-        document.querySelector('.message').textContent = "No Number! 🤣🤣"
+        if(score === 0) {
+            document.querySelector('.message').textContent = "You've lost! 😒";
+        }
+        else {
+            document.querySelector('.message').textContent = "No Number! 🤣🤣";
+            score--;
+            document.querySelector('.score').textContent = String(score);
+        }
     }
+
+    // When player wins
     else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = "Noiceeeeee! ❤❤❤"
+        if(score === 0) {
+            document.querySelector('.message').textContent = "You've lost! 😒";
+        }
+        else {
+            document.querySelector('.number').textContent = secretNumber;
+            document.querySelector('.message').textContent = "Noiceeeeee! ❤❤❤";
+
+            document.querySelector('body').style.backgroundColor = 'green';
+
+            document.querySelector('.number').style.width = '30rem'
+
+            if(score > highscore) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = highscore;
+            }
+        }
     }
+
+    // When guess is high
+    else if(guess > secretNumber) {
+        if(score === 0) {
+            document.querySelector('.message').textContent = "You've lost! 😒";
+        }
+        else {
+            document.querySelector('.message').textContent = "High!!!";
+            score--;
+            document.querySelector('.score').textContent = String(score);
+        }
+    }
+
+    // When guess is low
+    else if(guess < secretNumber) {
+        if(score === 0) {
+            document.querySelector('.message').textContent = "You've lost! 😒";
+        }
+        else {
+            document.querySelector('.message').textContent = "Lowwww!!!!!!!";
+            score--;
+            document.querySelector('.score').textContent = String(score);
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener
+('click', function() {
+    document.querySelector('.number').textContent = "?";
+    document.querySelector('.number').style.width = '15rem';
+
+    document.querySelector('.score').textContent = '20';
+
+    document.querySelector('.guess').value = "";
+
+    document.querySelector('.message').textContent = "Start guessing...";
+
+    document.querySelector('body').style.backgroundColor = '#222';
+
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    score = Number(document.querySelector('.score').textContent);
 })
